a58be0220bbb9ebb9779660e306b601347770d48
abbywake <71293588+abbywake@users.noreply.github.com>
2021-02-03 13:14:03 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Assignment3Movies.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3Movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult seeTheMovies()
+        {
+            return View(tempStorage.Applications);
+        }
+
+        public IActionResult bacon()
+        {
+            return View();
+        }
+
+        public IActionResult confirmation()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult addMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult addMovie(movie appResponse)
+        {
+            if(ModelState.IsValid)
+            {
+                if(appResponse.Title.ToUpper() != "INDEPENDENCE DAY")
+                {
+                    tempStorage.AddApplication(appResponse);
+                    return View("confirmation", appResponse);
+                }
+                else
+                {
+                    return View("confirmation", appResponse);
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+     
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/tempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3Movies.Models
+{
+    public static class tempStorage
+    {
+        private static List<movie> movieList = new List<movie>();
+
+        public static IEnumerable<movie> Applications => movieList;
+
+        public static void AddApplication(movie addmovies)
+        {
+            movieList.Add(addmovies);
+        }
+    }
+}
