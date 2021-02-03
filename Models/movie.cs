4148dd67f9d68aaa1232b3ebe027d060723b51c1
abbywake <71293588+abbywake@users.noreply.github.com>
2021-02-03 14:17:35 -0700

    fixed bugs and added css

--- Models/movie.cs
+++ Models/movie.cs
@@ -13,20 +13,19 @@ namespace Assignment3Movies.Models
 
         [Required]
         public string Title { get; set; }
-        
+
         [Required]
-        public int Year { get; set; }
-        
+        public string Year { get; set; }
         [Required]
         public string Director { get; set; }
 
         [Required]
         public string Rating { get; set; }
-
+      
         public bool Edited { get; set; }
-
+       
         public string LentTo { get; set; }
-
+      
         public string Notes { get; set; }
     }
 }
